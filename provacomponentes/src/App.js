9507d653bdaa001a5fd4acdc6ed1d0eead317458
Elisa-Kirnew <elisakirnew@gmail.com>
2023-09-19 17:43:09 +0000

    save01

---
+++ provacomponentes/src/App.js
@@ -0,0 +1,12 @@
+import Noticia from './componentes/noticia';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Noticia />
+    </div>
+  );
+}
+
+export default App;
---
+++ provacomponentes/src/componentes/noticia.js
@@ -0,0 +1,14 @@
+function Noticia(){
+
+    return(
+        <div className="App">
+        <h1>Relação da larissa Manoela com sua Familía</h1>
+        <p>Com o atrito com os pais, Larissa Manoela acabou passando os últimos meses mais próxima da família de André.
+           Nas redes sociais, ela aparece nas fotos com os sogros, com a cunhada, a diretora Camille Frambach, 
+           e com o noivo dela, o produtor musical Max Fernandez.</p>
+      </div>
+    )
+
+}
+
+export default Noticia
